0a67743567c9c7cf7735d003a1f4a148195b033f
Qundabaga <108898665+Qundabaga@users.noreply.github.com>
2022-07-07 22:44:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -64,28 +64,6 @@ if (isset($_POST['sku'])) {
                         </div>
                     </div>
                 </div>
-             
-
-
-<!-- Modal 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-    <form action="product.php" method="POST">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Update the product </h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body" id="update_details">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" name="update" class="btn btn-primary" id="update">Save changes</button>
-      </div>
-    </form>
-    </div>
-  </div>
-</div>-->
             </main>
         </div>
     </div>
